fc48626499c9de728ed5bb98049d697700f4a300
Viktor-Borg <vicke33@gmail.com>
2019-11-26 13:30:26 +0100

    basic controller

---
+++ TowerDefenseSpel/Controller.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace TowerDefenseSpel
+{
+    abstract class Controller<T>: Game where T : Controller<T>
+    {
+        public static T Singelton { get; private set; }
+
+        protected override void Initialize()
+        {
+            Singelton = (T)this;
+        }
+
+       
+    }
+}
